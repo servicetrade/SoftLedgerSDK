c13363d3ca6245ba00c209a8565fb84d4d7320a8
Robert Beier <106705021+robbeier-st@users.noreply.github.com>
2023-11-09 16:19:29 -0500

    changed item type to match db schema (#32)

--- src/types/items/Item.ts
+++ src/types/items/Item.ts
@@ -1,21 +1,21 @@
 import { CustomFields } from '../CustomFields';
 
 export type Item = {
-	_id: string;
-	externalIds: string;
+	_id: number;
+	externalIds?: string;
 	number: number;
 	name: string;
 	salePrice: string;
-	purchasePrice: string;
-	description: string;
-	sku: string;
-	lowStockNotification: boolean;
-	lowStockThreshold: string;
-	lowStockEmail: string;
+	purchasePrice?: string;
+	description?: string;
+	sku?: string;
+	lowStockNotification?: boolean;
+	lowStockThreshold?: string;
+	lowStockEmail?: string;
 	customFields: CustomFields;
-	InvoiceAccountId: string;
-	BillAccountId: string;
-	InventoryAccountId: string;
-	CogsAccountId: string;
-	inactive: boolean;
+	InvoiceAccountId?: number;
+	BillAccountId?: number;
+	InventoryAccountId?: number;
+	CogsAccountId?: number;
+	inactive?: boolean;
 };
