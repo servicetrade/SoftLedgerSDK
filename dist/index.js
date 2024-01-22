0cf10e6491f1fa31dec1fa2dd11b5606f9020922
Greg Timmonss <greg.timmons@servicetrade.com>
2024-01-22 13:06:47 -0500

    export ITem type enum

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,18 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
-exports.OrderStatus = exports.Status = exports.SoftLedgerAPI = void 0;
+exports.OrderStatus = exports.Status = exports.ItemType = exports.SoftLedgerAPI = void 0;
 var SoftLedgerAPI_1 = require('./SoftLedgerAPI');
 Object.defineProperty(exports, 'SoftLedgerAPI', {
 	enumerable: true,
 	get: function () {
 		return SoftLedgerAPI_1.SoftLedgerAPI;
+	},
+});
+var Item_1 = require('./types/items/Item');
+Object.defineProperty(exports, 'ItemType', {
+	enumerable: true,
+	get: function () {
+		return Item_1.ItemType;
 	},
 });
 var CreateSalesOrderRequest_1 = require('./types/salesOrders/CreateSalesOrderRequest');
