50a9ca2eedee0d115816b971a66f72aa9144290b
Greg Timmonss <greg.timmons@servicetrade.com>
2024-06-13 21:36:00 -0400

    Transitioning creates

---
+++ dist/types/stock/CreateTransferResponse.d.ts
@@ -0,0 +1,9 @@
+export declare type CreateTransferResponse = {
+	_id: number;
+	date: string;
+	quantity: string;
+	currency: string;
+	ItemId: number;
+	FromWarehouseId: number;
+	ToWarehouseId: number;
+};
---
+++ src/types/stock/CreateTransferResponse.ts
@@ -0,0 +1,9 @@
+export type CreateTransferResponse = {
+	_id: number;
+	date: string;
+	quantity: string;
+	currency: string;
+	ItemId: number;
+	FromWarehouseId: number;
+	ToWarehouseId: number;
+};
